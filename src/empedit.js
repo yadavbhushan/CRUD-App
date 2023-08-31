9c5d2158db2ac2c101ce7789002504c7074d0977
Bhushan kumar <bhushank.ug21.cs@nitp.ac.in>
2023-08-31 16:18:22 +0530

    First commit

---
+++ src/empedit.js
@@ -0,0 +1,96 @@
+import React, { useState,useEffect} from 'react'
+import  { Link,useNavigate,useParams} from 'react-router-dom';
+
+export default function Empedit(){
+    const {empid} = useParams();
+    const[id,idchange] =useState("");
+    const[name,namechange] =useState("");
+    const[email,emailchange] =useState("");
+    const[phone,phonechange] =useState("");
+    const[Roll_No,rollchange]=useState("");
+    const navigation = useNavigate();
+    useEffect(()=>{
+        fetch('http://localhost:8000/cruddata/'+empid).then((res)=>{
+        return res.json(); 
+        }).then((res)=>{
+            idchange(res.id);
+            namechange(res.name);
+            rollchange(res.Roll_No);
+            emailchange(res.email);
+            phonechange(res.phone);
+        }).catch((err)=>{
+            console.log(err);
+        })
+   
+    },[])
+        const handleSubmit=(e)=>{
+        e.preventDefault();
+        const empdata = {id,name,Roll_No,email,phone}
+       
+            fetch('http://localhost:8000/cruddata/'+empid,{
+            method:"PUT",
+            headers:{"content-type":"application/json"},
+            body:JSON.stringify(empdata)
+            }).then((res)=>{
+                alert('Record inserted');
+                navigation('/');
+            }).catch((err)=>{
+                console.log(err);
+            })
+           }
+  return (
+    <div>
+      <div className='row'>
+        <div className='container'>
+            <div className='card'>
+                <div className='card-title'>
+                    <h2>React Js Crud Tutorial</h2>
+                </div>
+                <div className='card-body'>
+                <div className='offset-lg-3 col-lg-6'>
+                <form onSubmit={handleSubmit}>
+                    <div className='row' style={{'textAlign':'left'}}>
+                    <div className='container'>
+
+                            <div className='col-lg-12'>
+                                <div className='form-group'>
+                                    <label>Name</label>
+                                    <input type='text' value={name} onChange={e=>namechange(e.target.value)} className='form-control' required></input>
+                                </div>
+                            </div>
+                            <div className='col-lg-12'>
+                                <div className='form-group'>
+                                    <label>Roll Number</label>
+                                    <input type='number' value={Roll_No} onChange={e=>rollchange(e.target.value)} className='form-control' required></input>
+                                </div>
+                            </div>
+                            <div className='col-lg-12'>
+                                <div className='form-group'>
+                                    <label>Email</label>
+                                    <input type='email' value={email} onChange={e=>emailchange(e.target.value)} className='form-control' required></input>
+                                </div>
+                            </div>
+                            
+                            <div className='col-lg-12'>
+                                <div className='form-group'>
+                                    <label>Phone Number</label>
+                                    <input type='number' value={phone} onChange={e=>phonechange(e.target.value)} className='form-control' required></input>
+                                </div>
+                            </div>
+                            <div className='col-lg-12'>
+                                <div className='form-group' style={{'textAlign':'center'}}>
+                                    <button type='submit' className='btn btn-success m-2'>Submit</button>
+                                    <Link to="/" className='btn btn-danger'>Back</Link>
+                                </div>
+                            </div>
+                    </div>
+                </div>
+</form>
+                </div>
+            </div>
+        </div>
+      </div>
+    </div>
+    </div>
+  )
+}
